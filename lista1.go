5fb6d2aebd771fedc3ff7384c1a4fef5c707bd34
Mateusz Tofil <40662705+tofiljr6@users.noreply.github.com>
2021-05-04 13:29:55 +0200

    lista 2 - zadanie 1 DONE

--- lista1.go
+++ lista1.go
@@ -54,7 +54,7 @@ func generateShortcuts(n, d int, e [][]int) [][]int {
 				tmp[0] = j
 				tmp[1] = k
 
-				if k == n-1 { // the last is receiver nth node
+				if k == n-1 || j == 0 { // the last is receiver nth node
 					break
 				}
 
@@ -87,9 +87,10 @@ func generateShortcutsBack(n, b int, e [][]int) [][]int {
 			j := rand.Intn(n)
 			k := rand.Intn(n)
 
-			if j-k > 1 {
+			if j-k >= 1 {
 				tmp[0] = j
 				tmp[1] = k
+				//fmt.Println(tmp)
 
 				if j == n-1 || k == 0 {
 					// the last is receiver nth node and producer is only one
@@ -167,6 +168,14 @@ func generateArrayHistoryPackages(k int) map[int][]int {
 	return mm
 }
 
+func generateArrayOfPackageLife(k, h int) map[int]int {
+	mm := make(map[int]int)
+	for i := 1; i < k+1; i++ {
+		mm[i] = h
+	}
+	return mm
+}
+
 func producer(k int, nc []chan int, vp map[int][]int, pv map[int][]int, sp chan string) {
 	// nc - nexts channels from current vertices
 	pack := 1
@@ -183,7 +192,7 @@ func producer(k int, nc []chan int, vp map[int][]int, pv map[int][]int, sp chan 
 	}
 }
 
-func node(id int, in <-chan int, nc []chan int, pv map[int][]int, vp map[int][]int, sp chan string) {
+func node(id int, in <-chan int, nc []chan int, pv map[int][]int, vp map[int][]int, sp chan string, h map[int]int) {
 	for {
 		p := <-in
 		pv[id] = append(pv[id], p)
@@ -196,19 +205,41 @@ func node(id int, in <-chan int, nc []chan int, pv map[int][]int, vp map[int][]i
 		sec := rand.Float64() * 2
 		time.Sleep(time.Second * time.Duration(sec))
 
-		randomChannel := rand.Intn(len(nc))
-		nc[randomChannel] <- p
+		var l = sync.Mutex{}
+		l.Lock()
+		h[p]--
+		plife := h[p]
+		l.Unlock()
+
+		if plife > 0 {
+			randomChannel := rand.Intn(len(nc))
+			nc[randomChannel] <- p
+		} else {
+			sp <- fmt.Sprint("\t\t\t\tŚmierć pakietu ", p)
+			l.Lock()
+			lifepack--
+			l.Unlock()
+		}
 	}
 }
 
-func consumer(k, id int, in <-chan int, d chan<- bool, pv map[int][]int, vp map[int][]int, sp chan string) {
-	for l := 0; l < k; l++ {
-		p := <-in
-		pv[id] = append(pv[id], p)
-		vp[p] = append(vp[p], id)
+func consumer(id int, in <-chan int, d chan<- bool, pv map[int][]int, vp map[int][]int, sp chan string) {
+	for {
+		select {
+		case p := <-in:
+			pv[id] = append(pv[id], p)
+			vp[p] = append(vp[p], id)
+			leftmargin := strings.Repeat("~", id)
+			sp <- fmt.Sprint(leftmargin, "\t\t\t\tPakiet ", p, " został odebrany")
+			lifepack--
+		case <-time.After(1 * time.Second):
+			// timeout - next loop
+			//fmt.Println("timeout", lifepack)
+		}
 
-		leftmargin := strings.Repeat("~", id)
-		sp <- fmt.Sprint(leftmargin, "Pakiet ", p, " został odebrany")
+		if lifepack == 0 {
+			break
+		}
 	}
 	d <- true
 }
@@ -225,16 +256,29 @@ func printGraph(e [][]int) {
 	}
 }
 
+var lifepack int = 1
+
 func main() {
 	// parse params from command line
 	nPtr := flag.Int("n", 0, "an int") // G(n-1) 0..n-1
 	dPtr := flag.Int("d", 0, "an int") // d <= n + 1
 	kPtr := flag.Int("k", 0, "an int") // k - number of packages
 	bPtr := flag.Int("b", 0, "an int") // back shortcuts
-
+	hPtr := flag.Int("h", 0, "an int") // life of package
 	flag.Parse()
 
-	if *nPtr > 0 && *dPtr > 0 && *kPtr > 0 && (*dPtr <= *nPtr+1) {
+	*nPtr = *nPtr + 2 // fast solution with sender and receiver - i will write better solution in the future
+
+	if (*nPtr > 0 && *dPtr > 0 && *kPtr > 0 && *bPtr > 0 && *hPtr > 0) && (*dPtr <= *nPtr-4) && (*bPtr <= *nPtr-2) {
+		/*
+			conditions
+			1* all of params must be positive
+			2* jump to forward must be d<n+1,
+				but i increment before if nPtr so its - 2 and first and the last one node
+				is only receiver and sender so -4 finally
+			3* jump to backward (shortcuts) must be b < n+1
+		*/
+
 		// create graph
 		e := generateEdges(*nPtr)
 		v := generateVertices(*nPtr)
@@ -243,6 +287,10 @@ func main() {
 		m := generateChannels(*nPtr)
 		vp := generateArrayHistoryVertices(*nPtr) // get history of packages in i-vertices
 		pv := generateArrayHistoryPackages(*kPtr)
+		h := generateArrayOfPackageLife(*kPtr, *hPtr)
+		fmt.Println(*hPtr)
+		fmt.Println(h)
+		lifepack = *kPtr
 
 		// printing graph
 		fmt.Println("GRAPH:", *bPtr)
@@ -260,16 +308,13 @@ func main() {
 		var done = make(chan bool)
 		var serverPrinter = make(chan string)
 
-		//fmt.Println("id producera", 0)
 		go producer(*kPtr, getNextChannels(getNexts(v[0], e), m), vp, pv, serverPrinter)
 
 		for i := 1; i < *nPtr-1; i++ {
-			//fmt.Println("id consumera", i)
-			go node(i, m[i], getNextChannels(getNexts(v[i], e), m), vp, pv, serverPrinter)
+			go node(i, m[i], getNextChannels(getNexts(v[i], e), m), vp, pv, serverPrinter, h)
 		}
 
-		//fmt.Println("id receivera", *nPtr-1)
-		go consumer(*kPtr, *nPtr-1, m[*nPtr-1], done, vp, pv, serverPrinter)
+		go consumer(*nPtr-1, m[*nPtr-1], done, vp, pv, serverPrinter)
 
 		var wg sync.WaitGroup
 		wg.Add(1)
